334d46b115da03bd0c3115df6f436c7140450f13
laskoviymishka <laskoviymishka@gmail.com>
2016-01-05 20:11:43 +0300

    11

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+            HorizontalLine HLine1 = new HorizontalLine(2,50,1,'_');
+            HLine1.Drow();
+
+            HorizontalLine HLine2 = new HorizontalLine(2, 50, 20, '_');
+            HLine2.Drow();
+
+            VerticalLine VLine1 = new VerticalLine(2, 20, 1, '|');
+            VLine1.Drow();
+
+            VerticalLine VLine2 = new VerticalLine(2, 20, 51, '|');
+            VLine2.Drow();
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList1;
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList1 = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList1.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach (Point p in pList1)
+            {
+                p.Draw();
+            }
+
+        }
+
+    }
+}
